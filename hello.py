19bfa77295c9be8745cdb87e9b64e166dd1db820
Ivan Ivakin <i_ivakin@mail.ru>
2021-06-14 18:22:07 +0000

    new update

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-def wsgi_app(env, start_responce):
+def wsgi_application(environ, start_responce):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
     data = environ['QUERY_STRING'].split('&')
-    body = '\r\n'.join(data).encode('utf-8')
+    body = '\n'.join(data).encode('utf-8')
     start_responce(status, headers)
     return [body]
