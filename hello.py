e0963b7ffa71471f8f1cd6dbeb3c7313af11baee
Ivan Ivakin <i_ivakin@mail.ru>
2021-06-14 18:31:30 +0000

    added r to python script

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ def wsgi_application(environ, start_responce):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
     data = environ['QUERY_STRING'].split('&')
-    body = '\n'.join(data).encode('utf-8')
+    body = '\r\n'.join(data).encode('utf-8')
     start_responce(status, headers)
     return body
