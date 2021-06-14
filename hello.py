454ca67daa2d051d3fcde26786b646bac0f5e9f7
Ivan Ivakin <i_ivakin@mail.ru>
2021-06-14 17:20:07 +0000

    corrected wsg_app

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 def wsgi_app(env, start_responce):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
-    body = '\r\n'.join(env['QUERY_STRING'].split('&'))
+    data = environ['QUERY_STRING'].split('&')
+    body = '\r\n'.join(data).encode('utf-8')
     start_responce(status, headers)
-    return body
+    return [body]
 
