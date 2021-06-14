3a8deb98ab57127c0beb6bf25d21ce3abe5266c6
Ivan Ivakin <68923400+Ivan-Ikavi@users.noreply.github.com>
2021-06-14 16:30:10 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+mv ~/stepic_web_application ~/web
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/defualt
 sudo /etc/init.d/nginx restart
