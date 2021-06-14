70ae992cb7854d8f2d60456dffe0ad82f198e228
Ivan Ivakin <i_ivakin@mail.ru>
2021-06-14 18:34:24 +0000

    next iteration

--- hello.py
+++ hello.py
@@ -4,4 +4,4 @@ def wsgi_application(environ, start_responce):
     data = environ['QUERY_STRING'].split('&')
     body = '\r\n'.join(data).encode('utf-8')
     start_responce(status, headers)
-    return body
+    return [body]
